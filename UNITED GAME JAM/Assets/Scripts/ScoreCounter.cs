ef90db9ee096b5348b5bd93d235ad8e3b1aef61c
Abdullah <anoor1375@gmail.com>
2020-06-27 15:01:27 -0500

    ScoreBoard
    Added score board and multiplier

---
+++ UNITED GAME JAM/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public float scoreAmount = 0;
+    public float multiplierAmmount = 1;
+    public float multiplierTimer = 10;
+    public float currentMultiplerTimer;
+
+    private void Start()
+    {
+        currentMultiplerTimer = multiplierTimer;
+    }
+    private void FixedUpdate()
+    {
+        currentMultiplerTimer -= Time.deltaTime;
+      
+    }
+
+    void Update()
+    {
+        scoreText.text = scoreAmount.ToString();
+
+    }
+
+    public void Addpoints(float pointAmmount)
+    {
+        //Check if mulitiplier has been reset
+        if (currentMultiplerTimer <= 0)
+        {
+            //set multiplier back
+            multiplierAmmount = 1;
+        }
+        else
+        {
+            //add multiplier
+            multiplierAmmount++;
+        }
+
+        //reset multiplier
+        currentMultiplerTimer = multiplierTimer;
+
+        //add points
+        scoreAmount += pointAmmount * multiplierAmmount;
+
+        //refrerance and change multiplier
+        FindObjectOfType<MultiplierBar>().ChangeMultiplier(multiplierAmmount);
+    }
+
+
+}
---
+++ UNITED GAME JAM/Assets/VFX/MultiplierBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MultiplierBar : MonoBehaviour
+{
+    public Image bar;
+    public RectTransform knob;
+    public TextMeshProUGUI multiplierAmmount;
+    ScoreCounter scoreCounter;
+    float multiplierText;
+    float timeAmmount;
+
+    private void Start()
+    {
+        scoreCounter = FindObjectOfType<ScoreCounter>();
+    }
+
+    private void Update()
+    {
+        ChangeTimmer(scoreCounter.currentMultiplerTimer);
+       // Debug.Log(FindObjectOfType<ScoreCounter>().currentMultiplerTimer);
+    }
+
+    public void ChangeTimmer(float time)
+    {
+        if (time <= 0)
+            time = 0;
+        //Debug.Log("changed timmer");
+        float ammount = (time / scoreCounter.multiplierTimer) * 270f / 360;
+        bar.fillAmount = ammount;
+        float knobPosition = ammount * 360;
+       
+        knob.localEulerAngles = new Vector3(0, 0, knobPosition);
+    }
+    public void ChangeMultiplier(float currentMultiplier)
+    {
+        multiplierAmmount.text = "X" +currentMultiplier.ToString();
+    }
+
+}
