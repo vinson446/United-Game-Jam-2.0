776e99db0269fbdb1ceed2ba4f6e6845e39caa3b
Vinson Kok <vinsonkok@yahoo.com>
2020-06-27 01:41:38 -0500

    Enemy 1.0

--- UNITED GAME JAM/Assets/Scripts/Enemy/Enemy.cs
+++ UNITED GAME JAM/Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,20 +11,45 @@ public class Enemy : MonoBehaviour
     public float moveSpeed = 5f;
 
     public int damage;
-
+    public Transform attackPoint;
+    public float attackRange;
     public float attackSpeed;
     float nextAttack;
+    public LayerMask enemyLayers;
+
+    PlayerController playerController;
+    NavMeshAgent navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerController = FindObjectOfType<PlayerController>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
         currentHP = maxHP;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, playerController.transform.position) < 1)
+        {
+            Attack();
+        }
+        else
+        {
+            MoveToPlayer();
+        }
+    }
+
+    void Attack()
+    {
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+    }
+
+    void MoveToPlayer()
+    {
+        navMeshAgent.SetDestination(playerController.transform.position);
     }
 
     public void TakeDamage(int d)
