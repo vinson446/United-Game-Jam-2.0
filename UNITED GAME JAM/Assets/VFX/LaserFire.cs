d7848395577600323e07b5e5653224e65dbd85ec
Abdullah <anoor1375@gmail.com>
2020-06-27 19:17:04 -0500

    Lazer bemes

---
+++ UNITED GAME JAM/Assets/VFX/LaserFire.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserFire : MonoBehaviour
+{
+    public GameObject lazer;
+    public GameObject firePoint;
+
+
+    private GameObject spawnedLazer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedLazer = Instantiate(lazer, firePoint.transform) as GameObject;
+        DisableLaser();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //click
+        //enablelaser
+
+       // hold click
+       //upadtelazer
+
+        //button up
+        //disable 
+    }
+
+    void EnableLaser()
+    {
+        spawnedLazer.SetActive(true);
+    }
+     void UpdateLaser()
+    {
+        if (firePoint != null)
+            spawnedLazer.transform.position=firePoint.transform.position;
+    }    
+
+    void DisableLaser()
+    {
+        spawnedLazer.SetActive(false);
+    }
+        
+}
+
+
+
+
+
