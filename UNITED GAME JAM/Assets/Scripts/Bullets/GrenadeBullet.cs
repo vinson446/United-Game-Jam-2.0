4336571c163f70dc73028fe770aebd5c31a27029
Abdullah <anoor1375@gmail.com>
2020-06-28 01:10:50 -0500

    Explosion Script and effect

--- UNITED GAME JAM/Assets/Scripts/Bullets/GrenadeBullet.cs
+++ UNITED GAME JAM/Assets/Scripts/Bullets/GrenadeBullet.cs
@@ -6,7 +6,9 @@ public class GrenadeBullet : MonoBehaviour
 {
     public int damage;
     public float range;
-
+    public float explosionRange=5f;
+    public GameObject explossionEffect;
+    float gernadeTimmer=5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,9 @@ public class GrenadeBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        gernadeTimmer -= Time.deltaTime;
+        if (gernadeTimmer <= 0)
+            Explode();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,11 +30,34 @@ public class GrenadeBullet : MonoBehaviour
             Enemy enemy = other.GetComponent<Enemy>();
             enemy.TakeDamage(damage);
 
-            Destroy(gameObject);
+            Explode();
         }
         else if (other.tag != "Player" && other.tag != "Bullet" && other.tag != "Left Bullet Spawn" && other.tag != "Right Bullet Spawn")
         {
-            Destroy(gameObject);
+            Explode();
         }
+
+    }
+
+    void Explode()
+    {
+        Debug.Log("BOMB GO BOOM");
+        Collider[] explosionZone = Physics.OverlapSphere(transform.position, explosionRange);
+        foreach(Collider nearByChar in explosionZone)
+        {
+            if (nearByChar.tag == "Enemy" )
+            {
+                Enemy enemy = nearByChar.GetComponent<Enemy>();
+                enemy.TakeDamage(damage);
+            }
+            if (nearByChar.tag == "Player")
+            {
+                PlayerController player = nearByChar.GetComponent<PlayerController>();
+                player.TakeDamage(damage);
+            }
+        }
+        GameObject boom = Instantiate(explossionEffect, transform.position,transform.rotation);
+        Destroy(boom, 2f);
+        Destroy(gameObject);
     }
 }
